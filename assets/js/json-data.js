573af730561ab88d6b243015a7b624761abce48c
SACHIN DUBEY <85615757+sachin-spitech@users.noreply.github.com>
2023-08-28 18:50:38 +0530

    Front-End Engineer Screening Assignment Submission
    1-done all point as per assignment

---
+++ assets/js/json-data.js
@@ -0,0 +1,32 @@
+[
+  {
+    "name": "Product1",
+    "category": "Electronic",
+    "price": 799.99,
+    "available": true
+  },
+  {
+    "name": "Product2",
+    "category": "Books",
+    "price": 19.99,
+    "available": true
+  },
+  {
+    "name": "Product3",
+    "category": "Home & Kitchen",
+    "price": 49.99,
+    "available": true
+  },
+  {
+    "name": "Product4",
+    "category": "Toys & Games",
+    "price": 29.99,
+    "available": true
+  },
+  {
+    "name": "Product5",
+    "category": "Electronic",
+    "price": 599.99,
+    "available": true
+  }
+]
---
+++ assets/js/main.js
@@ -0,0 +1,136 @@
+(function ($) {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select("#header");
+    let offset = header.offsetHeight;
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: "smooth",
+    });
+  };
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select("#header");
+  let selectTopbar = select("#topbar");
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add("header-scrolled");
+        if (selectTopbar) {
+          selectTopbar.classList.add("topbar-scrolled");
+        }
+      } else {
+        selectHeader.classList.remove("header-scrolled");
+        if (selectTopbar) {
+          selectTopbar.classList.remove("topbar-scrolled");
+        }
+      }
+    };
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
+  }
+
+  /*
+   * Gallery
+   */
+  $(".gallery").each(function () {
+    // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: "a", // the selector for gallery item
+      type: "image",
+      gallery: {
+        enabled: true,
+      },
+      closeBtnInside: true,
+      image: {
+        // options for image content type
+        titleSrc: "title",
+      },
+    });
+  });
+})(jQuery);
+
+function darken_screen(yesno) {
+  if (yesno == true) {
+    document.querySelector(".screen-darken").classList.add("active");
+  } else if (yesno == false) {
+    document.querySelector(".screen-darken").classList.remove("active");
+  }
+}
+
+function close_offcanvas() {
+  darken_screen(false);
+  document.querySelector(".mobile-offcanvas.show").classList.remove("show");
+  document.body.classList.remove("offcanvas-active");
+}
+
+function show_offcanvas(offcanvas_id) {
+  darken_screen(true);
+  document.getElementById(offcanvas_id).classList.add("show");
+  document.body.classList.add("offcanvas-active");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("[data-trigger]").forEach(function (everyelement) {
+    let offcanvas_id = everyelement.getAttribute("data-trigger");
+
+    everyelement.addEventListener("click", function (e) {
+      e.preventDefault();
+      show_offcanvas(offcanvas_id);
+    });
+  });
+
+  document.querySelectorAll(".btn-close").forEach(function (everybutton) {
+    everybutton.addEventListener("click", function (e) {
+      e.preventDefault();
+      close_offcanvas();
+    });
+  });
+
+  document
+    .querySelector(".screen-darken")
+    .addEventListener("click", function (event) {
+      close_offcanvas();
+    });
+});
